14adc8236d9fafbed6bede2d8379bbeb7fbee4a0
AACooper1 <alexisacooper1@gmail.com>
2025-06-03 15:41:08 -0600

    committing this before I delete read_seq() just in case

--- src/play_custom_music.c
+++ src/play_custom_music.c
@@ -3,56 +3,49 @@
 #include "recomputils.h"
 #include "recompconfig.h"
 #include "audio_api.c"
+#include "seq_tests/idkwhatsongthisis.xxd"
 
 #include <stdint.h>
-#include <stdio.h>
 #include <stdlib.h>
-
-// Code largely stolen from Garrett Cox from 2Ship on GitHub
-// https://gist.github.com/garrettjoecox/fcad0e942fd12c484328f787b9db3f93
-
-// Will be updated with original code later, but don't forget to credit him for the concept
 
 static uintptr_t sDevAddr = 0;
 static u8* sRamAddr = NULL;
 static size_t sSize = 0;
 static s32 sMedium = 0;
 
-static char *seq;
+static char *seq = NULL;
 
-RECOMP_HOOK("AudioLoad_SyncDma")
-void* get_seqPtr(uintptr_t devAddr, u8* ramAddr, size_t size, s32 medium)
-{
-    sDevAddr = devAddr;
-    sRamAddr = ramAddr;
-    sSize = size;
-    sMedium = medium;
-}
-
-void read_seq(char *seq)
+int read_seq()
 {
     FILE *seqFile = fopen("seq_tests/24.zseq", "rb");
-    char *buffer = NULL;
     size_t i, len;
 
     if (seq == NULL)
     {
         perror("Could not open seq_tests/24.zseq");
-        return EXIT_FAILURE
+        return EXIT_FAILURE;
     }
 
-    fseq(seqFile, )
+    fseek(seqFile, 0, SEEK_END);
+    len = ftell(seqFile);
+    rewind(seqFile);
+    seq = malloc(len + 1);
+    if(NULL == seq)
+    {
+        exit(-1);
+    }
 
-    fread(seq, 1, )
+
+    fread(seq, 1, len, seqFile);
+    fclose(seqFile);
+    
+    seq[len] = '\0';
 }
 
+read_seq();
 
-// For now, this just replaces the File Select sequence with the zseq provided by the file.
-RECOMP_HOOK_RETURN("AudioLoad_SyncDma")
-void injectSequence()
-{
-    if (sDevAddr == 0x62d80) { // File select sequence
-        Lib_MemSet(sRamAddr, 0, sSize);
-        Lib_MemCpy(sRamAddr, seq, sizeof(seq));
-    }
+RECOMP_IMPORT(".", void audio_apo_replace_sequence(u32 id, void* modAddr, size_t size));
+
+RECOMP_CALLBACK(".", audio_api_init) void my_audio_api_init() {
+    audio_api_replace_sequence(NA_BGM_FILE_SELECT, seq, sizeof(seq));
 }
