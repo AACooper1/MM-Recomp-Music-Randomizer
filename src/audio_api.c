bee190993c714c095a60be4500162598b8d3a191
AACooper1 <alexisacooper1@gmail.com>
2025-06-14 00:54:49 -0600

    Audiobanks working now. Added ULTRAKILL song as example :)

--- src/audio_api.c
+++ src/audio_api.c
@@ -2,6 +2,7 @@
 #include "global.h"
 #include "recomputils.h"
 #include "recompconfig.h"
+#include "debug_util.h"
 
 extern DmaHandler sDmaHandler;
 
@@ -11,12 +12,27 @@ RECOMP_DECLARE_EVENT(audio_api_init());
 
 // We need more exports for replacing samples, etc
 
-RECOMP_EXPORT void audio_api_replace_sequence(u32 id, void* modAddr, size_t size) {
+RECOMP_EXPORT void audio_api_replace_sequence(u32 id, u8 bankId, void* modAddr, size_t size) {
+    // Audioseq index table and entry for the sequence to replace
     AudioTable* table = (AudioTable*)gSequenceTable;
     AudioTableEntry* entry = &table->entries[id];
 
+    // Replace Audioseq index table entry
     entry->romAddr = (uintptr_t)modAddr - SEGMENT_ROM_START(Audioseq);
     entry->size = size; //ALIGN16(size);
+
+    // Sequence-to-Bank table entry
+    //      * Each entry is 2 sections:
+    //          - First byte is the number of banks in the seq's soundfont
+    //          - Following bytes are the list of those banks
+    //        All sequences except Sequence 0 have 0x01 as first byte. Sequence 0 has 0x02, so is 3 bytes long.
+    //      * Weird ass table header is 0x100 bytes long
+    //      * + 1 because we are accessing the second byte in the entry
+    //      * + 1 because Sequence 0 adds an extra
+    u8* seqFontEntry = &gSequenceFontTable[(id * 2) + 0x102];
+
+    // Replace Sequence-to-Bank table entry
+    *seqFontEntry = bankId;
 }
 
 RECOMP_HOOK("AudioLoad_Init") void on_AudioLoad_Init() {
@@ -74,7 +90,7 @@ s32 AudioApi_Dma_Rom(OSIoMesg* mesg, u32 priority, s32 direction, uintptr_t devA
 
 RECOMP_PATCH s32 AudioLoad_Dma(OSIoMesg* mesg, u32 priority, s32 direction, uintptr_t devAddr, void* ramAddr,
                                size_t size, OSMesgQueue* reqQueue, s32 medium, const char* dmaFuncType) {
-    if (IS_KSEG0(devAddr)) {
+    if (IS_KSEG0(devAddr)) { // If it's in RAM
         return AudioApi_Dma_Mod(mesg, priority, direction, devAddr, ramAddr, size, reqQueue, medium, dmaFuncType);
     } else {
         return AudioApi_Dma_Rom(mesg, priority, direction, devAddr, ramAddr, size, reqQueue, medium, dmaFuncType);
--- src/play_custom_music.c
+++ src/play_custom_music.c
@@ -2,19 +2,23 @@
 #include "global.h"
 #include "recomputils.h"
 #include "recompconfig.h"
+#include "debug_util.h"
 
-#include "seq_tests/idkwhatsongthisis.xxd"
+#include "../include/audio/load.h"
+#include "seq_tests/ULTRAKILL - The Fire is Gone.xxd"
 
 #include <stdint.h>
 #include <stdlib.h>
+
+extern uintptr_t AudioLoad_TrySyncLoadSampleBank(u32 sampleBankId, u32* outMedium, s32 noLoad);
 
 static uintptr_t sDevAddr = 0;
 static u8* sRamAddr = NULL;
 static size_t sSize = 0;
 static s32 sMedium = 0;
 
-RECOMP_IMPORT(".", void audio_api_replace_sequence(u32 id, void* modAddr, size_t size));
+RECOMP_IMPORT(".", void audio_api_replace_sequence(u32 id, u32 bankId, void* modAddr, size_t size));
 
-RECOMP_CALLBACK(".", audio_api_init) void my_audio_api_init() {
-    audio_api_replace_sequence(NA_BGM_FILE_SELECT, __24_zseq, sizeof(__24_zseq));
+RECOMP_CALLBACK(".", audio_api_init) void replace_sequence() {
+    audio_api_replace_sequence(NA_BGM_FILE_SELECT, __bank, __24_zseq, sizeof(__24_zseq));
 }
